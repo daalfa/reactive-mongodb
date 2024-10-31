8d834945a44938441a6f3bcee22887c7c3e3b362
Davi Alves Fagundes <davi.alves.fagundes@gmail.com>
2024-10-31 10:48:11 -0300

    Organize imports

--- src/main/java/com/davi/demo/restaurant/service/model/Cuisine.java
+++ src/main/java/com/davi/demo/restaurant/service/model/Cuisine.java
@@ -1,8 +1,8 @@
 package com.davi.demo.restaurant.service.model;
 
-import lombok.*;
+import lombok.Data;
+import lombok.NonNull;
 import org.springframework.data.annotation.Id;
-import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.index.TextIndexed;
 import org.springframework.data.mongodb.core.mapping.Document;
 
